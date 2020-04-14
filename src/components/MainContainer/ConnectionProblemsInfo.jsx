549ed61e53b2966d2477e2227740faef45f257b0
pedr0xx <pedrojoel28@gmail.com>
2020-04-14 14:09:46 -0400

    added media queries to search parameters

--- src/components/MainContainer/ConnectionProblemsInfo.jsx
+++ src/components/MainContainer/ConnectionProblemsInfo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+
 
 const ConnectionProblemsInfo = (props) => {
 	return(
 	<div className="connection-problems-info"> 
 		<h1>  Connection problems. Click here after a minute to keep loading</h1>
-		<button className="btn btn_reload" onClick={props.showMoreResults}> Reload</button>
+		<button className="btn btn_reload" onClick={props.showMoreResults}> <RefreshIcon/> </button>
 			</div>
 	)
 }
